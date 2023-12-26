5c7c2e0064eb14654e068a80be3a58a948d57205
kgdrjsen <gundoo3333@gmail.com>
2023-12-26 19:18:57 +0900

    초기

---
+++ app/src/main/java/com/android/together/Contants.kt
@@ -0,0 +1,9 @@
+package com.android.together
+
+object Contants {
+    const val USER_ID = "user_id"
+    const val USER_PASS = "user_pass"
+    const val USER_IMAGE = "user_image"
+    const val USER_NICKNAME = "user_nickname"
+    const val USER_NAME = "user_name"
+}
