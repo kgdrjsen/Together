094636b7c791ebda74e71d9ce5fdf873b91cdcef
KJ-Hyeon <bark052526@gmail.com>
2023-12-26 20:10:34 +0900

    프로젝트 초기 세팅

---
+++ app/src/main/java/com/android/together/User.kt
@@ -0,0 +1,15 @@
+package com.android.together
+
+object User {
+    var name: String = ""
+    var nickName: String = ""
+    var id: String = ""
+    var password: String = ""
+
+    fun addUser(name: String, nickName: String, id: String, password: String) {
+        this.name = name
+        this.nickName = nickName
+        this.id  = id
+        this.password = password
+    }
+}
