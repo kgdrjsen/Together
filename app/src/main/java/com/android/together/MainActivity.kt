b2745485228290bf0c9d2948339d722777a84f93
CodeNewbieee <choi2232@hanmail.net>
2023-12-29 11:12:35 +0900

    메인액티비티 빈칸으로 수정

--- app/src/main/java/com/android/together/MainActivity.kt
+++ app/src/main/java/com/android/together/MainActivity.kt
@@ -9,11 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.button).setOnClickListener {
-            var intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-        }
-
     }
 }
